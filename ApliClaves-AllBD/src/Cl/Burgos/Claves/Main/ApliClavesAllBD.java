d856e24509a0b37d1e7e6424a9c6498bb4f3d074
Marcelo <marchelo.bm@gmail.com>
2020-07-12 20:04:01 -0400

    v1.0 Iniciación del proyecto
    implementación de las Lib para iniciar el proyecto

---
+++ ApliClaves-AllBD/src/Cl/Burgos/Claves/Main/ApliClavesAllBD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cl.Burgos.Claves.Main;
+
+/**
+ *
+ * @author march
+ */
+public class ApliClavesAllBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
